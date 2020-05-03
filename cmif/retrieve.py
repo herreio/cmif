5eef66cbfa4c46418e0bcfd0063cb701a84ecc7f
herreio <git@herre.io>
2020-05-03 17:39:38 +0200

    cmif/retrieve.py updated

--- cmif/retrieve.py
+++ cmif/retrieve.py
@@ -24,15 +24,11 @@ def remote_file(url):
         print("http status code: ",
               str(response.status_code))
     except requests.exceptions.ConnectionError:
-        print("librair.protocols.http.get_request:")
         print("request failed! connection error...")
     except requests.exceptions.Timeout:
-        print("librair.protocols.http.get_request:")
         print("request failed! timeout... try later?")
     except requests.exceptions.TooManyRedirects:
-        print("librair.protocols.http.get_request:")
         print("request failed! bad url, try another one!")
     except requests.exceptions.RequestException:
-        print("librair.protocols.http.get_request:")
         print("request failed! don't know why...")
     return None
