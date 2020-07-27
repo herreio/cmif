df6a1b6dbfd3d73a6aa338b6fc1698689c8ecc6c
herreio <git@herre.io>
2020-07-27 16:42:02 +0200

    cmif.retrieve updated (stdout)

--- cmif/retrieve.py
+++ cmif/retrieve.py
@@ -19,19 +19,25 @@ def remote_file(url):
             try:
                 return etree.fromstring(response.content)
             except etree.ParseError:
+                print("cmif.retrieve.remote_file:")
                 print("failed to parse XML file!")
         else:
+            print("cmif.retrieve.remote_file:")
             print("requesting remote file failed!")
             print("url of request:")
             print(response.url)
             print("http status code: ",
                   str(response.status_code))
     except requests.exceptions.ConnectionError:
+        print("cmif.retrieve.remote_file:")
         print("request failed! connection error...")
     except requests.exceptions.Timeout:
+        print("cmif.retrieve.remote_file:")
         print("request failed! timeout... try later?")
     except requests.exceptions.TooManyRedirects:
+        print("cmif.retrieve.remote_file:")
         print("request failed! bad url, try another one!")
     except requests.exceptions.RequestException:
+        print("cmif.retrieve.remote_file:")
         print("request failed! don't know why...")
     return None
